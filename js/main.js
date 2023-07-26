53598ccb8f01fc936211d47491c371db3fe4e93d
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-07-27 00:33:59 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Создаем переменную, в которую положим кнопку меню
+let menuToggle = document.querySelector('#menu-toggle');
+// Создаем переменную, в которую положим меню
+let menu = document.querySelector('.sidebar');
+// отслеживаем клик по кнопке меню и запускаем функцию 
+menuToggle.addEventListener('click', function (event) {
+  // отменяем стандартное поведение ссылки
+  event.preventDefault();
+  // вешаем класс на меню, когда кликнули по кнопке меню 
+  menu.classList.toggle('visible');
+})
